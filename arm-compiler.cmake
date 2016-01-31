7eeda93f7d23d650a636a504deba047f0f36dccb
Paweł Stawicki <stawel+chealiCharger@gmail.com>
2016-01-31 19:04:22 +0100

    zmiany

--- arm-compiler.cmake
+++ arm-compiler.cmake
@@ -17,6 +17,10 @@ SET(CXXFLAGS "${CPU_FLAGS}  ${CTUNING} ${OTHER_FLAGS} -fno-rtti -fno-exceptions 
 SET(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS}   ${CFLAGS}   -ffunction-sections -fdata-sections")
 SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CXXFLAGS} -ffunction-sections -fdata-sections")
 
+
+set(LINKER_FLAGS "-T ${FX3_PATH}/fw_build/fx3_fw/fx3.ld -nostartfiles -Xlinker --gc-sections -Wl,-d -Wl,--no-wchar-size-warning  -Wl,--entry,CyU3PFirmwareEntry")
+set(CMAKE_EXE_LINKER_FLAGS "${LINKER_FLAGS}")
+
 #eclipse Linker:
 #arm-none-eabi-gcc -mcpu=arm926ej-s -marm -mthumb-interwork -O0 
 # -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections 
@@ -25,6 +29,3 @@ SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CXXFLAGS} -ffunction-sections -fdata-s
 # -L"/home/stawel/Cypress/cyfx3sdk/boot_lib/1_3_3/lib" 
 # -Wl,-Map,"BootLedBlink.map" -Wl,-d -Wl,-elf -Wl,--no-wchar-size-warning 
 # -Wl,--entry,Reset_Handler -o "BootLedBlink.elf"  ./cyfx_gcc_startup.o ./main.o   -lcyfx3boot -lc -lgcc
-
-set(LINKER_FLAGS "-T ${FX3_PATH}/fw_build/fx3_fw/fx3.ld -nostartfiles -Xlinker --gc-sections -Wl,-d -Wl,--no-wchar-size-warning  -Wl,--entry,CyU3PFirmwareEntry")
-set(CMAKE_EXE_LINKER_FLAGS "${LINKER_FLAGS}")
